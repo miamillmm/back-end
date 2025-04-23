4c4b218262f92778cf52fcd57120678fe112db41
Shahid Yaseen <skbangash184@gmail.com>
2025-04-23 18:23:33 +0500

    added CORS for syriasoug.com

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const io = initializeSocket(server);
 
 connectDB();
 
+app.use(express.json());
 // app.use(cors());
 // app.use(
 //   cors({
@@ -33,7 +34,7 @@ connectDB();
 app.use(express.json({ limit: '100mb' })); 
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cors({
-  origin: ['https://syriasouq.com','http://localhost:5174/'],
+  origin: ['https://syriasouq.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
