79949eb2a0941c721a354a2a7052ad2e881dfffa
Shahid Yaseen <skbangash184@gmail.com>
2025-05-22 19:59:42 +0500

    send message deleted

--- controllers/carController.js
+++ controllers/carController.js
@@ -7,129 +7,6 @@ const {
 
 // @desc    Add a new car
 // @route   POST /api/cars
-// const addCar = async (req, res) => {
-//   try {
-//     const {
-//       make,
-//       model,
-//       priceUSD,
-//       priceSYP,
-//       year,
-//       kilometer,
-//       engineSize,
-//       location,
-//       transmission,
-//       fuelType,
-//       exteriorColor,
-//       interiorColor,
-//       selectedFeatures,
-//       description,
-//     } = req.body;
-
-//     // Handle image uploads
-//     const images = req.files ? req.files.map((file) => file.filename) : [];
-//    console.log(images)
-//     // Convert features to an array if it's a string
-//     const parsedFeatures =
-//       typeof selectedFeatures === "string"
-//         ? JSON.parse(selectedFeatures)
-//         : selectedFeatures;
-//   console.log("test")
-//     const car = new Car({
-//       user: req.user.id, // Ensure user is authenticated
-//       make,
-//       model,
-//       priceUSD,
-//       priceSYP,
-//       year,
-//       kilometer,
-//       engineSize,
-//       location,
-//       transmission,
-//       fuelType,
-//       exteriorColor,
-//       interiorColor,
-//       features: parsedFeatures,
-//       description,
-//       images,
-//     });
-//     console.log(car)
-
-//     await car.save();
-
-//     // Notify the admin after saving the car
-//     // const subject = "New Car Listed for Approval";
-//     // const html = newCarNotificationTemplate(
-//     //   make,
-//     //   model,
-//     //   priceUSD,
-//     //   req.user.email
-//     // );
-//     // await sendEmail(process.env.EMAIL_USER, subject, "", html);
-
-//     res.status(201).json({ success: true, data: car });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
-// // @desc    Update car listing
-// // @route   PUT /api/cars/:id
-// const updateCar = async (req, res) => {
-//   try {
-//     const car = await Car.findById(req.params.id).populate(
-//       "user",
-//       "username phone"
-//     );
-//     if (!car) {
-//       return res.status(404).json({ success: false, message: "Car not found" });
-//     }
-
-//     console.log("Cars from hit: ", car, "from request User: ", req.user);
-
-//     // Ensure the authenticated user is the owner of the car
-//     if (
-//       car.user._id.toString() !== req.user.id &&
-//       req?.user?.role !== "admin"
-//     ) {
-//       return res.status(403).json({ success: false, message: "Unauthorized" });
-//     }
-
-//     // Update fields if provided
-//     car.make = req.body.make || car.make;
-//     car.model = req.body.model || car.model;
-//     car.priceUSD = req.body.priceUSD || car.priceUSD;
-//     car.priceSYP = req.body.priceSYP || car.priceSYP;
-//     car.year = req.body.year || car.year;
-//     car.kilometer = req.body.kilometer || car.kilometer;
-//     car.engineSize = req.body.engineSize || car.engineSize;
-//     car.location = req.body.location || car.location;
-//     car.transmission = req.body.transmission || car.transmission;
-//     car.fuelType = req.body.fuelType || car.fuelType;
-//     car.exteriorColor = req.body.exteriorColor || car.exteriorColor;
-//     car.interiorColor = req.body.interiorColor || car.interiorColor;
-//     car.features = req.body.features
-//       ? JSON.parse(req.body.features)
-//       : car.features;
-//     car.description = req.body.description || car.description;
-//     car.status = 'pending';
-
-//     // Update images if new ones are uploaded
-//     if (req.files && req.files.length > 0) {
-//       car.images = req.files.map((file) => file.filename);
-//     }
-
-//     await car.save();
-
-//     // if (car.status !== "pending" || car.status !== "sold")
-//     // await approveCarListing(car?.user?.email, car.make);
-
-//     res.json({ success: true, data: car });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
 const addCar = async (req, res) => {
   try {
     const {
@@ -151,13 +28,13 @@ const addCar = async (req, res) => {
 
     // Handle image uploads
     const images = req.files ? req.files.map((file) => file.filename) : [];
-
+   console.log(images)
     // Convert features to an array if it's a string
     const parsedFeatures =
       typeof selectedFeatures === "string"
         ? JSON.parse(selectedFeatures)
         : selectedFeatures;
-
+  console.log("test")
     const car = new Car({
       user: req.user.id, // Ensure user is authenticated
       make,
@@ -175,20 +52,20 @@ const addCar = async (req, res) => {
       features: parsedFeatures,
       description,
       images,
-      status: 'pending', // Set status to pending for admin approval
     });
+    console.log(car)
 
     await car.save();
 
     // Notify the admin after saving the car
-    const subject = "New Car Listed for Approval";
-    const html = newCarNotificationTemplate(
-      make,
-      model,
-      priceUSD,
-      req.user.email
-    );
-    await sendEmail(process.env.EMAIL_USER, subject, "", html);
+    // const subject = "New Car Listed for Approval";
+    // const html = newCarNotificationTemplate(
+    //   make,
+    //   model,
+    //   priceUSD,
+    //   req.user.email
+    // );
+    // await sendEmail(process.env.EMAIL_USER, subject, "", html);
 
     res.status(201).json({ success: true, data: car });
   } catch (error) {
@@ -207,6 +84,8 @@ const updateCar = async (req, res) => {
     if (!car) {
       return res.status(404).json({ success: false, message: "Car not found" });
     }
+
+    console.log("Cars from hit: ", car, "from request User: ", req.user);
 
     // Ensure the authenticated user is the owner of the car
     if (
@@ -241,21 +120,141 @@ const updateCar = async (req, res) => {
 
     await car.save();
 
-    // Notify the admin after updating the car
-    const subject = "Car Listing Updated for Approval";
-    const html = newCarNotificationTemplate(
-      car.make,
-      car.model,
-      car.priceUSD,
-      req.user.email
-    );
-    await sendEmail(process.env.EMAIL_USER, subject, "", html);
+    // if (car.status !== "pending" || car.status !== "sold")
+    // await approveCarListing(car?.user?.email, car.make);
 
     res.json({ success: true, data: car });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+// const addCar = async (req, res) => {
+//   try {
+//     const {
+//       make,
+//       model,
+//       priceUSD,
+//       priceSYP,
+//       year,
+//       kilometer,
+//       engineSize,
+//       location,
+//       transmission,
+//       fuelType,
+//       exteriorColor,
+//       interiorColor,
+//       selectedFeatures,
+//       description,
+//     } = req.body;
+
+//     // Handle image uploads
+//     const images = req.files ? req.files.map((file) => file.filename) : [];
+
+//     // Convert features to an array if it's a string
+//     const parsedFeatures =
+//       typeof selectedFeatures === "string"
+//         ? JSON.parse(selectedFeatures)
+//         : selectedFeatures;
+
+//     const car = new Car({
+//       user: req.user.id, // Ensure user is authenticated
+//       make,
+//       model,
+//       priceUSD,
+//       priceSYP,
+//       year,
+//       kilometer,
+//       engineSize,
+//       location,
+//       transmission,
+//       fuelType,
+//       exteriorColor,
+//       interiorColor,
+//       features: parsedFeatures,
+//       description,
+//       images,
+//       status: 'pending', // Set status to pending for admin approval
+//     });
+
+//     await car.save();
+
+//     // Notify the admin after saving the car
+//     const subject = "New Car Listed for Approval";
+//     const html = newCarNotificationTemplate(
+//       make,
+//       model,
+//       priceUSD,
+//       req.user.email
+//     );
+//     await sendEmail(process.env.EMAIL_USER, subject, "", html);
+
+//     res.status(201).json({ success: true, data: car });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// };
+
+// // @desc    Update car listing
+// // @route   PUT /api/cars/:id
+// const updateCar = async (req, res) => {
+//   try {
+//     const car = await Car.findById(req.params.id).populate(
+//       "user",
+//       "username phone"
+//     );
+//     if (!car) {
+//       return res.status(404).json({ success: false, message: "Car not found" });
+//     }
+
+//     // Ensure the authenticated user is the owner of the car
+//     if (
+//       car.user._id.toString() !== req.user.id &&
+//       req?.user?.role !== "admin"
+//     ) {
+//       return res.status(403).json({ success: false, message: "Unauthorized" });
+//     }
+
+//     // Update fields if provided
+//     car.make = req.body.make || car.make;
+//     car.model = req.body.model || car.model;
+//     car.priceUSD = req.body.priceUSD || car.priceUSD;
+//     car.priceSYP = req.body.priceSYP || car.priceSYP;
+//     car.year = req.body.year || car.year;
+//     car.kilometer = req.body.kilometer || car.kilometer;
+//     car.engineSize = req.body.engineSize || car.engineSize;
+//     car.location = req.body.location || car.location;
+//     car.transmission = req.body.transmission || car.transmission;
+//     car.fuelType = req.body.fuelType || car.fuelType;
+//     car.exteriorColor = req.body.exteriorColor || car.exteriorColor;
+//     car.interiorColor = req.body.interiorColor || car.interiorColor;
+//     car.features = req.body.features
+//       ? JSON.parse(req.body.features)
+//       : car.features;
+//     car.description = req.body.description || car.description;
+//     car.status = req.body.status || car.status;
+//     // Update images if new ones are uploaded
+//     if (req.files && req.files.length > 0) {
+//       car.images = req.files.map((file) => file.filename);
+//     }
+
+//     await car.save();
+
+//     // Notify the admin after updating the car
+//     const subject = "Car Listing Updated for Approval";
+//     const html = newCarNotificationTemplate(
+//       car.make,
+//       car.model,
+//       car.priceUSD,
+//       req.user.email
+//     );
+//     await sendEmail(process.env.EMAIL_USER, subject, "", html);
+
+//     res.json({ success: true, data: car });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// };
 
 // @desc    Get all cars
 // @route   GET /api/cars
